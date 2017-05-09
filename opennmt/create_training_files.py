219cc9259ea88a00e1f4bca8fb7338373bf466f5
suri <swigder@gmail.com>
2017-05-09 21:01:18 +0200

    removing duplicate headlines

--- opennmt/create_training_files.py
+++ opennmt/create_training_files.py
@@ -18,16 +18,30 @@ def write_samples_to_opennmt_format(samples_training, samples_validation, locati
             myfile.write('\n'.join(headlines))
 
 
+def remove_duplicate_headlines(samples):
+    headlines = set()
+    for sample in samples:
+        if sample.headline in headlines:
+            samples.remove(sample)
+        headlines.add(sample.headline)
+
+
 if __name__ == '__main__':
     er_dir = '../../news-retriever/data'
-    samples = read_event_registry_data(*[path.join(er_dir, f) for f in listdir(er_dir) if f.endswith('.json')])
+    samples = []
+    samples += read_event_registry_data(*[path.join(er_dir, f) for f in listdir(er_dir) if f.endswith('.json')])
     samples += read_crowdflower_economic_data('../../data/Full-Economic-News-DFE-839861.csv')
     samples += read_crowdflower_wikipedia_data('../../data/News-article-wikipedia-DFE.csv')
     reuters_dir = '../../data/reuters21578'
     samples += read_reuters_data(*[path.join(reuters_dir, f) for f in listdir(reuters_dir) if f.endswith('.sgm')])
-    shuffle(samples)
+    print('Started with', len(samples), 'samples...')
+    remove_duplicate_headlines(samples)
+    print('Trimmed to', len(samples), 'samples by removing duplicate headlines')
+    for sample in samples:
+        if 'Turkish singer' in sample.headline:
+            print(sample)
     total_samples = len(samples)
-    training_samples = int(total_samples * .75)
+    training_samples = int(total_samples * .8)
     print('Total samples: {}, training {}, validation {}', total_samples, training_samples, total_samples - training_samples)
     write_samples_to_opennmt_format(samples_training=samples[:training_samples],
                                     samples_validation=samples[training_samples:],
