c95c6ed52771795ff28faed8bd4baff915260cbd
suri <swigder@gmail.com>
2017-05-09 21:47:21 +0200

    saving epochs

--- opennmt.sh
+++ opennmt.sh
@@ -4,4 +4,4 @@ th tools/tokenize.lua < $DATA_DIR/training-target.txt > $DATA_DIR/training-targe
 th tools/tokenize.lua < $DATA_DIR/validation-samples.txt > $DATA_DIR/validation-samples.txt.tok
 th tools/tokenize.lua < $DATA_DIR/validation-target.txt > $DATA_DIR/validation-target.txt.tok
 th preprocess.lua -train_src $DATA_DIR/training-samples.txt.tok -train_tgt $DATA_DIR/training-target.txt.tok -valid_src $DATA_DIR/validation-samples.txt.tok -valid_tgt $DATA_DIR/validation-target.txt.tok -src_seq_length 100 -tgt_seq_length 20 -save_data $DATA_DIR/own.t7-train.t7
-th train.lua -data $DATA_DIR/own.t7-train.t7-train.t7 -save_model ../data/train.t7 -save_every 200 -gpuid 1
+th train.lua -data $DATA_DIR/own.t7-train.t7-train.t7 -save_model ../models/train.t7 -save_every_epochs 4 -gpuid 1
