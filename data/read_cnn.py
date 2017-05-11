139f33d2d55cad79458ef09e373b23ba45fccd15
hongyi chen <hongyic@kth.se>
2017-05-11 16:03:03 +0200

    able to get corpus from cnn

--- data/read_cnn.py
+++ data/read_cnn.py
@@ -1,107 +1,123 @@
-import csv
-import json
-import random
+from itertools import chain
 
-import re
-from bs4 import BeautifulSoup
-
+from lxml import html
 
 from data.language import Sample
 
 
-def read_event_registry_data(*files):
-    # output from news-retriever project
-    source_suffixes = [' \| .*', ' - Amateur Photographer', ' - Kabayan Weekly', ' - Daily Journal',
-                       ' - Mobile News Online', ' - The Lethbridge Herald - News and Sports from around Lethbridge',
-                       ' - Vanguard News', ' - Futurity', ' - CFN Media', ' - Kuwait Times', ' - Voice of Asia Online',
-                       ' - Cyprus Mail', ' - Daily Post Nigeria', ' - MoneyWeek', ' - BBC News']
+def ParseHtml(story, corpus): # from
+    """Parses the HTML of a news story.
+    Args:
+      story: The raw Story to be parsed.
+      corpus: Either 'cnn' or 'dailymail'.
+    Returns:
+      A Story containing URL, paragraphs and highlights.
+    """
 
-    clean = lambda h: re.sub('|'.join(source_suffixes), '', h)
+    parser = html.HTMLParser(encoding='utf-8')
+    tree = html.document_fromstring(story, parser=parser)
 
-    articles = []
-    processed = set()
-    for file in files:
-        with open(file) as data_file:
-            data = json.load(data_file)
-            [articles.append(Sample(headline=clean(v['info']['title']), body=v['info']['body']))
-             for (k, v) in data.items()
-             if 'error' not in v.keys()
-             and v['info']['id'] not in processed
-             and v['info']['url'] is not 'http://www.theaustralian.com.au/video']
-            [processed.add(v['info']['id'])
-             for (k, v) in data.items()
-             if 'error' not in v.keys()]
-    return articles
+    # Elements to delete.
+    delete_selectors = {
+        'cnn': [
+            '//blockquote[contains(@class, "twitter-tweet")]',
+            '//blockquote[contains(@class, "instagram-media")]'
+        ],
+        'dailymail': [
+            '//blockquote[contains(@class, "twitter-tweet")]',
+            '//blockquote[contains(@class, "instagram-media")]'
+        ]
+    }
 
+    # Paragraph exclusions: ads, links, bylines, comments
+    cnn_exclude = (
+        'not(ancestor::*[contains(@class, "metadata")])'
+        ' and not(ancestor::*[contains(@class, "pullquote")])'
+        ' and not(ancestor::*[contains(@class, "SandboxRoot")])'
+        ' and not(ancestor::*[contains(@class, "twitter-tweet")])'
+        ' and not(ancestor::div[contains(@class, "cnnStoryElementBox")])'
+        ' and not(contains(@class, "cnnTopics"))'
+        ' and not(descendant::*[starts-with(text(), "Read:")])'
+        ' and not(descendant::*[starts-with(text(), "READ:")])'
+        ' and not(descendant::*[starts-with(text(), "Join us at")])'
+        ' and not(descendant::*[starts-with(text(), "Join us on")])'
+        ' and not(descendant::*[starts-with(text(), "Read CNNOpinion")])'
+        ' and not(descendant::*[contains(text(), "@CNNOpinion")])'
+        ' and not(descendant-or-self::*[starts-with(text(), "Follow us")])'
+        ' and not(descendant::*[starts-with(text(), "MORE:")])'
+        ' and not(descendant::*[starts-with(text(), "SPOILER ALERT:")])')
 
-def read_reuters_data(*files):
-    # http://www.daviddlewis.com/resources/testcollections/reuters21578/
-    articles = []
-    for file in files:
-        try:
-            soup = BeautifulSoup(open(file), 'html.parser')
-            for article in soup.find_all('reuters'):
-                if article.title and article.body:
-                    articles.append(Sample(headline=article.title.text, body=article.body.text))
-        except:
-            print('Error processing file:', file)
-    return articles
+    dm_exclude = (
+        'not(ancestor::*[contains(@id,"reader-comments")])'
+        ' and not(contains(@class, "byline-plain"))'
+        ' and not(contains(@class, "byline-section"))'
+        ' and not(contains(@class, "count-number"))'
+        ' and not(contains(@class, "count-text"))'
+        ' and not(contains(@class, "video-item-title"))'
+        ' and not(ancestor::*[contains(@class, "column-content")])'
+        ' and not(ancestor::iframe)')
 
+    paragraph_selectors = {
+        'cnn': [
+            '//div[contains(@class, "cnnContentContainer")]//p[%s]' % cnn_exclude,
+            '//div[contains(@class, "l-container")]//p[%s]' % cnn_exclude,
+            '//div[contains(@class, "cnn_strycntntlft")]//p[%s]' % cnn_exclude
+        ],
+        'dailymail': [
+            '//div[contains(@class, "article-text")]//p[%s]' % dm_exclude
+        ]
+    }
 
-def read_crowdflower_data(*files, headline_name, text_name):
-    articles = []
-    for file in files:
-        with open(file) as csv_file:
-            data_reader = csv.reader(csv_file, delimiter=',', quotechar='"')
-            headline, body = None, None
-            for sample in data_reader:
-                if headline is None:
-                    headline = sample.index(headline_name)
-                    body = sample.index(text_name)
-                    continue
-                articles.append(Sample(headline=sample[headline], body=sample[body]))
-    return articles
+    # Highlight exclusions.
+    he = (
+        'not(contains(@class, "cnnHiliteHeader"))'
+        ' and not(descendant::*[starts-with(text(), "Next Article in")])')
+    highlight_selectors = {
+        'cnn': [
+            '//*[contains(@class, "el__storyhighlights__list")]//li[%s]' % he,
+            '//*[contains(@class, "cnnStryHghLght")]//li[%s]' % he,
+            '//*[@id="cnnHeaderRightCol"]//li[%s]' % he
+        ],
+        'dailymail': [
+            '//h1/following-sibling::ul//li'
+        ]
+    }
 
-def read_cnn_data(*files):
-    articles = []
-    for file in files:
-        try:
-            soup = BeautifulSoup(open(file), 'html.parser')
-            title = soup.find('title').text
-            body=soup.find('body').text
-            articles.append(Sample(headline=title,body=body))
-        except:
-            print('Error processing file:', file)
-    return articles
+    def ExtractText(selector):
+        """Extracts a list of paragraphs given a XPath selector.
+        Args:
+          selector: A XPath selector to find the paragraphs.
+        Returns:
+          A list of raw text paragraphs with leading and trailing whitespace.
+        """
 
-def read_crowdflower_economic_data(*files):
-    # https://www.crowdflower.com/wp-content/uploads/2016/03/Full-Economic-News-DFE-839861.csv
-    return read_crowdflower_data(*files, headline_name='headline', text_name='text')
+        xpaths = map(tree.xpath, selector)
+        elements = list(chain.from_iterable(xpaths))
+        paragraphs = [e.text_content() for e in elements]
 
+        # Remove editorial notes, etc.
+        if corpus == 'cnn' and len(paragraphs) >= 2 and '(CNN)' in paragraphs[1]:
+            paragraphs.pop(0)
 
-def read_crowdflower_wikipedia_data(*files):
-    # https://www.crowdflower.com/wp-content/uploads/2016/03/News-article-wikipedia-DFE.csv
-    return read_crowdflower_data(*files, headline_name='article', text_name='newdescp')
+        paragraphs = map(str.strip, paragraphs)
+        paragraphs = [s for s in paragraphs if s and not str.isspace(s)]
 
+        return paragraphs
+
+    for selector in delete_selectors[corpus]:
+        for bad in tree.xpath(selector):
+            bad.getparent().remove(bad)
+
+    paragraphs = ExtractText(paragraph_selectors[corpus])
+
+    return '\n'.join(paragraphs)
 
 if __name__ == '__main__':
-    samples = read_event_registry_data('../../news-retriever/data/data.json',
-                                       '../../news-retriever/data/data-1000.json',
-                                       '../../news-retriever/data/data-20170428-144403.json',
-                                       '../../news-retriever/data/data-20170428-144520.json',
-                                       '../../news-retriever/data/data-20170428-144543.json')
-    print('\nEvent Registry, ', len(samples), 'samples')
-    print(random.choice(samples))
-
-    samples = read_crowdflower_economic_data('../../data/Full-Economic-News-DFE-839861.csv')
-    print('\nCrowdflower economic', len(samples), 'samples')
-    print(random.choice(samples))
-
-    samples = read_reuters_data('../../data/reuters21578/reut2-000.sgm')
-    print('\nReuters', len(samples), 'samples')
-    print(random.choice(samples))
-
-    samples = read_cnn_data('./cnn/00a2aef1e18d125960da51e167a3d22ed8416c09.html')
-    print('\ncnn', len(samples), 'samples')
-    print(samples)
+    url = './cnn/00a2aef1e18d125960da51e167a3d22ed8416c09.html'
+    with open(url) as f:
+        story_html = f.read()
+    corpus = 'cnn'
+    a = ParseHtml(story_html, corpus)
+    sample = Sample(headline='', body=a)
+    print(a)
 
